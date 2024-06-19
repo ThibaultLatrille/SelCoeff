b1f4e03b484a42098ac73b82409350194a9b6688
Thibault Latrille <thibault.latrille@unil.ch>
2024-06-19 15:39:14 +0200

    Renamed diff files

--- manuscript/compile.sh
+++ manuscript/compile.sh
@@ -1,1 +1,3 @@
-latexdiff main.tex main-revision.tex > diff.tex
+latexdiff main-PNAS.tex main-PNAS-revision.tex > diff-PNAS-revision.tex
+latexdiff main-PNAS-revision.tex main-PNAS-revision-2.tex > diff-PNAS-revision-2.tex
+latexdiff main-PLOS.tex main.tex > diff-PLOS-revision.tex
