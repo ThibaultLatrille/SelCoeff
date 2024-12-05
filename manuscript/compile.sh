0a9416e15873168871a52f1ce25582e705dc1a88
T. Latrille <thibault.latrille@ens-lyon.org>
2024-12-05 14:33:22 +0100

    Revision PLOS Genetics round 2

--- manuscript/compile.sh
+++ manuscript/compile.sh
@@ -1,3 +1,4 @@
 latexdiff main-PNAS.tex main-PNAS-revision.tex > diff-PNAS-revision.tex
 latexdiff main-PNAS-revision.tex main-PNAS-revision-2.tex > diff-PNAS-revision-2.tex
-latexdiff main-PLOS.tex main.tex > diff-PLOS-revision.tex
+latexdiff main-PLOS.tex main-PLOS-revision.tex > diff-PLOS-revision.tex
+latexdiff main-PLOS-revision-plos2015.tex main-PLOS-revision-2-plos2015.tex > diff-PLOS-revision-2.tex
